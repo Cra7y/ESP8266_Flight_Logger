70b60a2a108bb637b3ea85a528dd63007b9c9a6a
Cra7y <cra7y.com@gmail.com>
2021-01-24 22:31:12 +0100

    Cleaned up code and added weightless detection

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,18 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
 
+// Defines
+#define SERVOPIN 16
+#define WEIGHTLESSTRESHOLD 1.0
+
+// Global functions
+bool isWeightless(float sum) {
+  if (sum <= WEIGHTLESSTRESHOLD) {
+    return true;
+  }
+  return false;
+}
+
 // Servo global variables
 Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
@@ -19,56 +31,67 @@ void activateServo() {
 }
 
 
-// ADXL 345 global variables
+// ADXL 345 global variables and functions
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
-
-void checkWeightless() {
-  
+// Get event
+sensors_event_t getEvent() {
+  sensors_event_t event; 
+  accel.getEvent(&event);
+}
+// Get sum of forces
+float getSum(sensors_event_t event) {
+  return sqrt(pow(event.acceleration.x, 2)+pow(event.acceleration.y, 2)+pow(event.acceleration.z, 2));
+}
+// Log sensor event
+void logEvent(sensors_event_t event) {
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print(" ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print(" ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print(" ");
+  Serial.print("SUM: "); Serial.print(getSum(event)); Serial.print(" "); Serial.println("m/s^2 ");
 }
 
+// Global variables
 unsigned long nextChange = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  // Setup libs
+  myservo.attach(SERVOPIN);  // attaches the servo on pin 2 (D4) to the servo object
+  Serial.begin(115200); // Start serial
+  if(!accel.begin()) { // Start accelerometer
+    // There was a problem detecting the ADXL345 ... check your connections
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  // Setup pins
   pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
-  myservo.attach(16);  // attaches the servo on pin 2 (D4) to the servo object
-  Serial.begin(115200);
-  Serial.println("Started");
-
-  /* Initialise the sensor */
- if(!accel.begin())
- {
- /* There was a problem detecting the ADXL345 ... check your connections */
- Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
- while(1);
- }
  
- /* Set the range to whatever is appropriate for your project */
- accel.setRange(ADXL345_RANGE_16_G);
- // displaySetRange(ADXL345_RANGE_8_G);
- // displaySetRange(ADXL345_RANGE_4_G);
- // displaySetRange(ADXL345_RANGE_2_G);
+  // Set the range of accelerometer to +-16G
+  accel.setRange(ADXL345_RANGE_16_G);
 
- Serial.println("");
+  Serial.println("Started");
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  // Get a new ADXL345 sensor event
+  sensors_event_t event = getEvent();
+  float sum = getSum(event);
   
+  // Check if device is weightless (at the top of arch)
+  if (isWeightless(sum)) {
+    activateServo();
+  }
+
+  // Update LED and log current sensor data
   if (nextChange <= millis()) {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-
-  /* Get a new sensor event */ 
- sensors_event_t event; 
- accel.getEvent(&event);
- 
- /* Display the results (acceleration is measured in m/s^2) */
- Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print(" ");
- Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print(" ");
- Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print(" "); Serial.print("m/s^2 "); Serial.print(" ");
- Serial.println(sqrt(pow(event.acceleration.x, 2)+pow(event.acceleration.y, 2)+pow(event.acceleration.z, 2)));
-
+    logEvent(event);
     nextChange = nextChange + 1000;
   }
+
+  delay(100);
   
 }
