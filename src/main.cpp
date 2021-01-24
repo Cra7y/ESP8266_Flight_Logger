91f105076502ff637a8e536e33c80962fe9468c9
Cra7y <cra7y.com@gmail.com>
2021-01-24 18:00:44 +0100

    Servo and Builtin LED is now functional

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,29 @@ Servo myservo;  // create servo object to control a servo
 
 int pos = 0;    // variable to store the servo position
 
+unsigned long nextChange = 0;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(16);  // attaches the servo on pin 2 (D4) to the servo object
+  Serial.begin(115200);
+  Serial.println("Started");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  
+  if (nextChange <= millis()) {
+    Serial.println("Switching led");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    if (pos == 0) {
+      pos = 180;
+    } else {
+      pos = 0;
+    }
+    myservo.write(pos);
+    nextChange = nextChange + 1000;
+  }
+  
 }
