0684e08ebb55aa865783abafc4ebc5219c1e3115
Cra7y <cra7y.com@gmail.com>
2021-01-29 17:05:03 +0100

    Finished adding support for all components. Cleaned up code by splitting long sections to individual functions.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 #include <SPI.h>
 
 // Include other libs
-#include <servo.h> // Servo
-#include <Wire.h>  // I2C
-#include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
-#include <Adafruit_BMP085.h>
+#include <servo.h>              // Servo
+#include <Wire.h>               // I2C
+#include <Adafruit_Sensor.h>    // Unified sensor
+#include <Adafruit_ADXL345_U.h> // ADXL345
+#include <Adafruit_BMP085.h>    // BMP085
+#include <FastLED.h>            // WS2812
 /* #endregion */
 
 /* #region  Defines */
@@ -15,12 +16,15 @@
 #define SERVOHOME 180  // The position where the servo holds parachute in
 #define SERVORELEASE 0 // The position where the servo releases parachute
 
+#define NUM_LEDS 1 // Number of WS2812 Leds
+#define DATA_PIN 0 // WS2812 pin
+
 #define WEIGHTLESSTRESHOLD 1.0 // The treshold for when we asume weightlessness in m/s^2
 #define INFLIGHTTRESHOLD 30    // The treshold for when we asume device has left the ramp in m/s^2
 #define LANDEDTRESHOLD 15      // The treshold for when we asume device is landed (laying still) in m/s^2
 
 // Startup
-#define STARTUPDELAY 10000     // Time before closing servo on startup
+#define STARTUPDELAY 10000 // Time before closing servo on startup
 // Led delay at diferent states (in ms)
 #define LEDSTATE0 500
 #define LEDSTATE1 250
@@ -37,11 +41,11 @@
 /* #region  Global variables */
 // Flight variables
 #ifdef DEBUG
-  int state = RUNDEBUG;
+int state = RUNDEBUG;
 #else
-  int state = STARTUP;
+int state = STARTUP;
 #endif
-bool top = false;      // True after max altitude is reached
+bool top = false; // True after max altitude is reached
 unsigned int startTime;
 unsigned int topTime;
 int landedCounter = 0;
@@ -56,31 +60,36 @@ bool logOverflow = false;
 
 /* #region  LED global variables and functions */
 unsigned long nextChange = 0; //LED Control
-int ledDelay = LEDSTATE0;          // Delay between led change
+int ledDelay = LEDSTATE0;     // Delay between led change
 int ledState = 0;             // Current state of LED
 // Change led delay
-void changeLedDelay(int newDelay) {
+void changeLedDelay(int newDelay)
+{
   nextChange = millis() + newDelay;
   ledDelay = newDelay;
 }
-void setLedState(int newState) {
-  if (newState != ledState) {
+void setLedState(int newState)
+{
+  if (newState != ledState)
+  {
     ledState = newState;
-    switch (ledState) {
-      case 0:
-        changeLedDelay(LEDSTATE0);
-        break;
-      case 1:
-        changeLedDelay(LEDSTATE1);
-        break;
-      case 2:
-        changeLedDelay(LEDSTATE2);
-        break;
+    switch (ledState)
+    {
+    case 0:
+      changeLedDelay(LEDSTATE0);
+      break;
+    case 1:
+      changeLedDelay(LEDSTATE1);
+      break;
+    case 2:
+      changeLedDelay(LEDSTATE2);
+      break;
     }
   }
 }
 // Update LED
-void updateLed() {
+void updateLed()
+{
   if (nextChange <= millis())
   {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
@@ -88,7 +97,8 @@ void updateLed() {
   }
 }
 // Set LED to off
-void ledOFF() {
+void ledOFF()
+{
   digitalWrite(LED_BUILTIN, LOW);
 }
 /* #endregion */
@@ -154,6 +164,10 @@ double startPressure;
 double altitude;
 /* #endregion */
 
+/* #region  WS2812 global variables and functions */
+CRGB leds[NUM_LEDS];
+/* #endregion */
+
 /* #region  Global functions */
 // Check if devie is weightless
 bool isWeightless(float sum)
@@ -164,68 +178,120 @@ bool isWeightless(float sum)
   }
   return false;
 }
+// Map float values
+float mapf(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+// Gets battery voltage
+float getBatteryVoltage() {
+  return mapf(float(analogRead(A0)), 13.0, 1024.0, 0.0, 4.18);
+}
+// Create log
+void createLog() { // TODO: Ghange to something that makes sense in the field
+  Serial.println("Log");
+  for (int i = 0; i < sizeI; i++) {
+    if (flightLog[i][0] == 0.0) { //TODO: Reconsider conditions
+      break;
+    }
+    Serial.print("Time: ");
+    Serial.print(flightLog[i][0]);
+    Serial.print("ms ");
+    Serial.print("X: ");
+    Serial.print(flightLog[i][1]);
+    Serial.print(" ");
+    Serial.print("Y: ");
+    Serial.print(flightLog[i][2]);
+    Serial.print(" ");
+    Serial.print("Z: ");
+    Serial.print(flightLog[i][3]);
+    Serial.print(" ");
+    Serial.print("SUM: ");
+    Serial.print(flightLog[i][4]);
+    Serial.print(" ");
+    Serial.println("m/s^2 ");
+  }
+  Serial.print("Rocket reached a topheight of ");
+  Serial.print(altitude);
+  Serial.print("m at ");
+  Serial.print(topTime);
+  Serial.println("ms");
+  Serial.println("End of log");
+}
+
+// Runs once when STARTUPDELAY is reached
+void onStartupTimeout() {
+  changeLedDelay(1000);
+  myservo.write(SERVOHOME);
+  startPressure = bmp.readPressure(); // Measure start pressure (Needed to calculate relative preasure later)
+  state = WAITINGFORLAUNCH;
+}
+// Runs once on launch
+void onLaunch() {
+  state = INFLIGHT;     // Set device to inFligt mode
+  startTime = millis(); // Save time of launch
+  ledOFF();             // Turn led off
+}
+// Runs once when top is reached
+void onTop(unsigned int functionStart) {
+  topTime = functionStart - startTime;        // Caluculate time since launch
+  altitude = bmp.readAltitude(startPressure); // Measure max height
+  top = true;
+  Serial.println("Releasing parachute");
+  myservo.write(SERVORELEASE);
+}
+// Runs once when landed
+void onLand() {
+  state = LANDED;
+  landedCounter = 0;
+  top = false;
+
+  createLog();
+}
+
 // Runs in flight
-void runInFlight() {
+void runInFlight()
+{
   //Runs while device in inflight
   unsigned int functionStart = millis(); // Keeps track of when function started
-  
+
   // Get a new ADXL345 sensor event
   sensors_event_t event;
   accel.getEvent(&event);
   float sum = getSum(event);
-  
+
   // Check if we reached maximum altitude
-  if (sum < WEIGHTLESSTRESHOLD && !top) {
-    topTime = functionStart - startTime; // Caluculate time since launch
-    altitude = bmp.readAltitude(startPressure); // Measure max height
-    top = true;
-    Serial.println("Releasing parachute");
-    myservo.write(SERVORELEASE);
+  if (sum < WEIGHTLESSTRESHOLD && !top)
+  {
+    onTop(functionStart);
   }
 
   // Log acceleration variables
-  if (logCursor < sizeI) {
-    flightLog[logCursor][0] = functionStart - startTime;  // Time since launch
-    flightLog[logCursor][1] = event.acceleration.x;       // Acceleration X
-    flightLog[logCursor][2] = event.acceleration.y;       // Acceleration X
-    flightLog[logCursor][3] = event.acceleration.z;       // Acceleration X
-    flightLog[logCursor][4] = sum;                        // Acceleration resulting force
+  if (logCursor < sizeI)
+  {
+    flightLog[logCursor][0] = functionStart - startTime; // Time since launch
+    flightLog[logCursor][1] = event.acceleration.x;      // Acceleration X
+    flightLog[logCursor][2] = event.acceleration.y;      // Acceleration X
+    flightLog[logCursor][3] = event.acceleration.z;      // Acceleration X
+    flightLog[logCursor][4] = sum;                       // Acceleration resulting force
     logCursor++;
-  } else {
+  }
+  else
+  {
     logOverflow = true;
   }
 
   // If acceleration is less than LANDEDTRESHOLD for 20 iterations we asume that we have landed
-  if (landedCounter < 20 && sum < LANDEDTRESHOLD) {
+  if (landedCounter < 20 && sum < LANDEDTRESHOLD)
+  {
     landedCounter++;
-  } else if (landedCounter >= 20 && sum < LANDEDTRESHOLD) {
-    state = LANDED;
-    landedCounter = 0;
-    top = false;
-    /* TODO: log max height and end log (Compile log)
-    serialRadio.print("Rocket reached a topheight of ");
-    serialRadio.print(altitude);
-    serialRadio.print("m at ");
-    serialRadio.print(topTime);
-    serialRadio.println("ms");
-    serialRadio.println("E");
-    */
-    Serial.println("Log");
-    for (int i = 0; i < sizeI; i++) {
-      if (flightLog[i][0] == 0.0) {
-        break;
-      }
-      Serial.print("Time: "); Serial.print(flightLog[i][0]); Serial.print("ms ");
-      Serial.print("X: "); Serial.print(flightLog[i][1]); Serial.print(" ");
-      Serial.print("Y: "); Serial.print(flightLog[i][2]); Serial.print(" ");
-      Serial.print("Z: "); Serial.print(flightLog[i][3]); Serial.print(" ");
-      Serial.print("SUM: "); Serial.print(flightLog[i][4]); Serial.print(" ");
-      Serial.println("m/s^2 ");
-    }
-    Serial.print("Rocket reached a topheight of "); Serial.print(altitude); Serial.print("m at "); Serial.print(topTime); Serial.println("ms");
-    Serial.println("End of log");
-    
-  } else if (sum > LANDEDTRESHOLD) {
+  }
+  else if (landedCounter >= 20 && sum < LANDEDTRESHOLD)
+  {
+    onLand();
+  }
+  else if (sum > LANDEDTRESHOLD)
+  {
     landedCounter = 0;
   }
 
@@ -233,23 +299,26 @@ void runInFlight() {
   long int functionTime = millis() - functionStart; // Calculate execution time of function
   int remainingDelay = 50 - functionTime;
   // Only delay if function took less than 50ms
-  if (remainingDelay > 0) {
+  if (remainingDelay > 0)
+  {
     delay(remainingDelay);
   }
 }
 // Runs while waiting to be launched
-void waitForLaunch() {
+void waitForLaunch()
+{
   // Get a new ADXL345 sensor event
   sensors_event_t event;
   accel.getEvent(&event);
   float sum = getSum(event);
 
   // Wait until the device hits acceleration defined as INFLIGHTTRESHOLD
-  if (sum > INFLIGHTTRESHOLD) {
-    state = INFLIGHT;           // Set device to inFligt mode
-    startTime = millis();       // Save time of launch
-    ledOFF();                   // Turn led off
-  } else {
+  if (sum > INFLIGHTTRESHOLD)
+  {
+    onLaunch();
+  }
+  else
+  {
     delay(50); // Device was not launched, so we wait
   }
 }
@@ -263,6 +332,7 @@ void setup()
   Serial.begin(115200);     // Start serial
   beginAccel();             // Start accelerometer
   bmp.begin();              // Start barometer
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // FastLED - GRB ordering is assumed
 
   // Setup pins
   pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
@@ -275,8 +345,9 @@ void setup()
 
   Serial.println("Started");
 
-  if (state == RUNDEBUG) {
-    //startPressure = bmp.readPressure();
+  if (state == RUNDEBUG)
+  {
+    startPressure = bmp.readPressure();
   }
 }
 
@@ -287,18 +358,22 @@ void loop()
     case STARTUP: {
       delay(50);
       updateLed();
-      
+
       unsigned int curTime = millis();
-      if (curTime > STARTUPDELAY) {
-        changeLedDelay(1000);
-        myservo.write(SERVOHOME);
-        startPressure = bmp.readPressure(); // Measure start pressure (Needed to calculate relative preasure later)
-        state = WAITINGFORLAUNCH;
-      } else if (curTime > (STARTUPDELAY/3)*2) {
+      if (curTime > STARTUPDELAY)
+      {
+        onStartupTimeout();
+      }
+      else if (curTime > (STARTUPDELAY / 3) * 2)
+      {
         setLedState(2);
-      } else if (curTime > (STARTUPDELAY/3)) {
+      }
+      else if (curTime > (STARTUPDELAY / 3))
+      {
         setLedState(1);
-      } else {
+      }
+      else
+      {
         setLedState(0);
       }
       break;
@@ -319,10 +394,28 @@ void loop()
       break;
     }
     case RUNDEBUG: {
+      // Check WS2812
+      leds[0] = CRGB::Red;
+      FastLED.show();
+      delay(250);
+
+      leds[0] = CRGB::Green;
+      FastLED.show();
+      delay(250);
+
+      leds[0] = CRGB::Blue;
+      FastLED.show();
+      delay(250);
+
+      leds[0] = CRGB::Black;
+      FastLED.show();
+      delay(250);
+
+      // Check BMP185
       Serial.print("Temperature = ");
       Serial.print(bmp.readTemperature());
       Serial.println(" Celsius");
- 
+
       Serial.print("Pressure = ");
       Serial.print(bmp.readPressure());
       Serial.println(" Pascal");
@@ -330,9 +423,34 @@ void loop()
       Serial.print("Altitude = ");
       Serial.print(bmp.readAltitude(startPressure));
       Serial.println(" Meters");
- 
+
+      // Check ADXL345
+      sensors_event_t event;
+      accel.getEvent(&event);
+      float sum = getSum(event);
+      
+      Serial.print("Sum of acceleration = ");
+      Serial.print(sum);
+      Serial.println(" m/s^2");
+
+      Serial.print("Analog reading = ");
+      Serial.print(analogRead(A0));
+      Serial.println(" int");
+
+      Serial.print("battery = ");
+      Serial.print(getBatteryVoltage());
+      Serial.println(" v");
+
+      // Check Servo
+      Serial.println("Moving servo home");
+      myservo.write(SERVOHOME);
+      delay(500);
+      Serial.println("Moving servo to release");
+      myservo.write(SERVORELEASE);
+      delay(500);
+
       Serial.println();
-      delay(5000);
+      break;
     }
   }
 
